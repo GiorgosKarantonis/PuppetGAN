f4e3f70deafcda36095de7e0d0d2b99583016899
Giorgos Karantonis <gkaranto@redhat.com>
2020-09-25 16:13:14 -0400

    minor changes

--- PuppetGAN/noisy_synth.py
+++ PuppetGAN/noisy_synth.py
@@ -20,6 +20,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+'''
+	Adds noise to the background of the synthetic images.
+'''
+
 import os
 
 import numpy as np
